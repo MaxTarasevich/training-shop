a74f03a545b301b7606cdd83d2d177dbdaf9b64f
Max <maxtarasevich@gmail.com>
2022-02-06 16:58:15 +0300

    feat: add TailwindCss && TypeScript

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return <header className='py-20 bg-slate-600'>
+      <div className="container">
+          header
+      </div>
+  </header>;
+};
+
+export default Header;

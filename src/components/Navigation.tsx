c7dc928697b47f5a8b760693d4a5d4feb151a762
Max <maxtarasevich@gmail.com>
2022-02-08 23:25:26 +0300

    feat: add nav component

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 
 import Logo from '../assets/icons/main/Logo.svg'
 
+import Seach from '../SVGcomponent/Seach';
+import Globe from '../SVGcomponent/Globe';
+import User from '../SVGcomponent/User';
+import Bag from '../SVGcomponent/Bag';
+
 const Navigation = () => {
-  return <nav className='nav pb-5 pt-4  flex'>
+  return <nav className='nav'>
+    <div className="container pb-5 pt-4  flex justify-between">
       <a className='nav__logo' href="home">
         <img src={Logo} alt="Logo" />
       </a>
 
-      <ul className='nav__menu flex'>
+      <ul className='nav__menu flex space-x-4 text-title4 text-dark'>
         <li><a href="about">About Us</a></li>
         <li><a href="women">Women</a></li>
         <li><a href="men">Men</a></li>
@@ -18,21 +24,21 @@ const Navigation = () => {
         <li><a href="contact">Contact</a></li>
       </ul>
 
-      <ul className="nav__icons flex">
+      <ul className="nav__icons flex space-x-5">
         <li>
-
+          <Seach />
         </li>
         <li>
-
+          <Globe />
         </li>
         <li>
-
+          <User />
         </li>
         <li>
-          
+          <Bag />
         </li>
       </ul>
-      
+    </div>
   </nav>;
 };
 
